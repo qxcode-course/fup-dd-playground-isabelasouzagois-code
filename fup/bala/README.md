cd11083e90a728e57253e44b7b68fedf4ccf5141
isabelasouzagois-code <isabelasouzagois@alu.ufc.br>
2026-03-22 19:15:04 +0000

    finalizadas

--- fup/bala/README.md
+++ fup/bala/README.md
@@ -1,5 +1,5 @@
 
- # Distância entre dois pontos
+# Distância entre dois pontos
 
 ![_](../../.tko/cache/e2bc1de5ac270eaa50476053dc6bb1f9ff79f7ba/base/bala/cover.jpg)
 
--- fup/bala/go/draft.go
+++ fup/bala/go/draft.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("qxcode")
+	var x1, y1, x2, y2 float64
+	fmt.Scan(&x1, &y1, &x2, &y2)
+	dx := x2 - x1
+	dy := y2 - y1
+	soma := dx*dx + dy*dy
+	distancia := math.Sqrt(soma)
+	fmt.Printf("%.2f\n", distancia)
 }
