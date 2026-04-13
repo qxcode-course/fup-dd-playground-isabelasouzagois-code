2d90efabe8bec5f8a458c7e9a4c6d32af6202535
isabelasouzagois-code <isabelasouzagois@alu.ufc.br>
2026-04-13 13:16:34 +0000

    mais mais

---
+++ fup/pares/src/go/draft.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+    var a,b,soma int
+    fmt.Scan(&a,&b)
+    if a>b{
+        fmt.Println("invalido")
+        return
+    }
+    for i:= a;i<=b;i++{
+        if i%2==0{
+            soma+=i
+        }
+    }
+    fmt.Println(soma)
+}
